4e11a560df9efebfd0d0bbfb3cd7a8a9cfc1cb28
RitwikSingh28 <itsritwik26@gmail.com>
2023-01-06 22:48:28 +0530

    add inc-salary functionality

--- contracts/Pension.sol
+++ contracts/Pension.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract Pension{
+contract Pension {
     address owner; //company admin
     address employee;
     uint initalSalary;
@@ -11,41 +11,49 @@ contract Pension{
 
     //we will give address and initalSalary at time of deploying smart contract
     //we are deploying contract when the employee is hired
-    constructor(address _person,uint _initSal){
+    constructor(address _person, uint _initSal) {
         owner = msg.sender;
         employee = _person;
         initalSalary = _initSal;
         currentSalary = _initSal;
-
     }
 
     //function to increase salary
     //only deployer should be able to do this
     //do some calculation for getting current salary
     //set currentSalary to calculated salary
-    function increaseSalary(uint yrsOfWork,uint _initSalary) external {
-        
+    function increaseSalary(uint yrsOfWork) external {
+        require(msg.sender == owner, "Only deployer can change salary");
+        uint _currentSalary = currentSalary;
+        uint _percentIncr = 0;
+
+        //scope of improving the increase_salary logic
+        if (yrsOfWork <= 1) {
+            _percentIncr = 5;
+        } else if (yrsOfWork <= 5) {
+            _percentIncr = 10;
+        } else if (yrsOfWork <= 10) {
+            _percentIncr = 15;
+        } else {
+            _percentIncr = 20;
+        }
+
+        _currentSalary =
+            _currentSalary +
+            ((_currentSalary / 100) * _percentIncr);
+        currentSalary = _currentSalary;
     }
 
     //logic to get this month's salary from company to contract as per calculation done above
 
     //this function transfers half of salary to employee account
     //use call()
-    function sendAmount() external {
-        
-    }
+    function sendAmount() external {}
 
     //counter to store pension
-    function storePension()external {
-
-    }
+    function storePension() external {}
 
     //check if pensiongiving date occured
     //transfer pension equally at certain intervals
     //reset pension giving date to next month
-
-    
-
-
-
 }
